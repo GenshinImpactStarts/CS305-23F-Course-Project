e754b062e9120e2bbb348b33b41833be3103fd5e
Calmness <1628281262@qq.com>
2023-12-13 09:41:10 +0800

    feat(header.py): shift values into constructs, not completed yet.

--- m_http/header.py
+++ m_http/header.py
@@ -3,8 +3,41 @@ import datetime
 
 __all__ = 'Header',
 
+# No need for you to call
+class HeadBuilder:
+    def __init__(self):
+        # the fields you must give
+        self.status_code=None
+        self.server_name=None
+        self.content_type=None
+        self.content_length=None
+        self.connection=None
+
+        # optional fields
+
+class Headers:
+    def __init__(self):
+        pass
 
 class Header:
+
+    head_builder=HeadBuilder()
+    server_name=None
+
+    def initialize_headbuilder(self):
+        self.head_builder=HeadBuilder()
+        pass
+
+    
+    def __init__(self, server_name="GenShinImpactStarts"):
+        self.initialize_headbuilder()
+        self.server_name=server_name
+        pass
+
+
+    def get_headbuilder(self):
+        return self.head_builder
+    
     # only parse the Request-Line!
     def parse_request_line(request):
         lines = request.split('\r\n')
@@ -13,59 +46,61 @@ class Header:
         return method, path, version
     
     # parse the others!
+
     def parse_request_headers(request):
-            lines = request.split('\r\n')
-            header_lines = lines[1:]
-            headers = {}
-            for line in header_lines:
-                if line:
-                    key, value = line.split(': ', 1)
-                    headers[key] = value
+        lines = request.split('\r\n')
+        header_lines = lines[1:]
+        headers = Headers()
+        for line in header_lines:
+            if line:
+                key, value = line.split(': ', 1)
+                
+                setattr(headers, key.strip(), value.strip())
 
-            return headers
+        return headers
     
-    def generate_response_headers(status_code, server_name=None, content_type=None, keep_alive=None, connection=None, age=None, date=None, x_cache_info=None, content_length=None):
+    # before you use this method, please edit the headbuilder correctly!
+    def generate_response_headers(self):
+        string_builder=[]
+        string_builder.extend(f"HTTP/1.1 {self.head_builder.status_code} {StatusCode.get_description(self.head_builder.status_code)}\r\n") 
+        
+        string_builder.extend(f"Server: {self.head_builder.server_name}\r\n")
 
-        response_headers = f"HTTP/1.1 {status_code} {StatusCode.get_description(status_code)}\r\n"
-
-        if server_name is not None:
-            response_headers += f"Server: {server_name}\r\n"
-        else: response_headers += f"Server: GenShinImpactStarts\r\n"
-
-        if content_type is not None:
-            response_headers += f"Content-Type: {content_type}\r\n" # maybe 'text/html; charset=utf-8'
+        if string_builder.content_type is not None:
+            string_builder.extend(f"Content-Type: {self.head_builder.content_type}\r\n") # maybe 'text/html; charset=utf-8'
         else: response_headers += f"Content-Type: text/html; charset=utf-8\r\n"
 
-        if keep_alive is not None:
-            response_headers += f"Keep-Alive: timeout={keep_alive[0]}, max={keep_alive[1]}\r\n" # like 'timeout=5, max=1000'
+        if string_builder.keep_alive is not None:
+            response_headers += f"Keep-Alive: timeout={self.head_builder.keep_alive[0]}, max={self.head_builder.keep_alive[1]}\r\n" # like 'timeout=5, max=1000'
         else: pass # Who cares?
 
-        if connection is not None:
-            if connection==0:
+        if string_builder.connection is not None:
+            if string_builder.connection==0:
                 response_headers += f"Connection: close\r\n"
-            if connection==1:
+            if string_builder.connection==1:
                 response_headers += f"Connection: Keep-Alive\r\n"
         else: response_headers += f"Connection: Keep-Alive\r\n"
 
-        if age is not None:
-            response_headers += f"Age: {age}\r\n"
+        if string_builder.age is not None:
+            response_headers += f"Age: {string_builder.age}\r\n"
         else: pass # I don't know :(
         
-        if date is not None:
-            response_headers += f"Date: {date}\r\n"
+        if string_builder.date is not None:
+            response_headers += f"Date: {string_builder.date}\r\n"
         else: 
             current_datetime = datetime.datetime.now()
             http_date = current_datetime.strftime("%a, %d %b %Y %H:%M:%S GMT") # Not Sure!
             response_headers += f"Date: {http_date}"
 
 
-        if x_cache_info is not None:
-            response_headers += f"X-Cache-Info: {x_cache_info}\r\n"
-        else: response_headers += f"X-Cache-Info: caching\r\n" #not sure
+        if string_builder.x_cache_info is not None:
+            response_headers += f"X-Cache-Info: {string_builder.x_cache_info}\r\n"
+        else: pass #response_headers += f"X-Cache-Info: caching\r\n" #not sure
 
-        if content_length is not None:
-            response_headers += f"Content-Length: {content_length}\r\n"
-        else: response_headers += f"Content-Length: 0\r\n"
+        if string_builder.content_length is not None:
+            response_headers += f"Content-Length: {string_builder.content_length}\r\n"
+        else: pass #response_headers += f"Content-Length: 0\r\n"
 
+        self.initialize_headbuilder()# reset the headbuilder
         return response_headers
 
