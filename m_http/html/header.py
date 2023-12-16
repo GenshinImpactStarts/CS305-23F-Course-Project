8c8c561d8fbe278f9c3d3c400cecfe103762f68b
Calmness <1628281262@qq.com>
2023-12-17 00:50:00 +0800

    fix(header.py): fix lots of things

--- m_http/html/header.py
+++ m_http/html/header.py
@@ -12,7 +12,9 @@ class HeadBuilder:
         self.content_type=None
         self.content_length=None
         self.connection=None
-
+        self.x_cache_info=None
+        self.date=None
+        self.age=None
         # optional fields
 
 class Headers:
@@ -39,17 +41,17 @@ def http_header_to_python(header):
 
 class Header:
 
-    head_builder=HeadBuilder()
     server_name=None
 
     def initialize_headbuilder(self):
         self.head_builder=HeadBuilder()
+        self.head_builder.server_name=self.server_name
         pass
 
     
     def __init__(self, server_name="GenShinImpactStarts"):
-        self.initialize_headbuilder()
         self.server_name=server_name
+        self.initialize_headbuilder()
         pass
 
 
@@ -80,45 +82,44 @@ class Header:
     # before you use this method, please edit the headbuilder correctly!
     def generate_response_headers(self):
         string_builder=[]
-        string_builder.extend(f"HTTP/1.1 {self.head_builder.status_code} {StatusCode.get_description(self.head_builder.status_code)}\r\n") 
-        
-        string_builder.extend(f"Server: {self.head_builder.server_name}\r\n")
+        string_builder.append(f"HTTP/1.1 {self.head_builder.status_code} {StatusCode.get_description(self.head_builder.status_code)}\r\n") 
+        string_builder.append(f"Server: {self.head_builder.server_name}\r\n")
 
-        if string_builder.content_type is not None:
-            string_builder.extend(f"Content-Type: {self.head_builder.content_type}\r\n") # maybe 'text/html; charset=utf-8'
-        else: response_headers += f"Content-Type: text/html; charset=utf-8\r\n"
+        if self.head_builder.content_type is not None:
+            string_builder.append(f"Content-Type: {self.head_builder.content_type}\r\n") # maybe 'text/html; charset=utf-8'
+        else: string_builder.append(f"Content-Type: text/html; charset=utf-8\r\n")
 
-        if string_builder.keep_alive is not None:
-            response_headers += f"Keep-Alive: timeout={self.head_builder.keep_alive[0]}, max={self.head_builder.keep_alive[1]}\r\n" # like 'timeout=5, max=1000'
+        if self.head_builder.keep_alive is not None:
+            string_builder.append(f"Keep-Alive: timeout={self.head_builder.keep_alive[0]}, max={self.head_builder.keep_alive[1]}\r\n") # like 'timeout=5, max=1000'
         else: pass # Who cares?
 
-        if string_builder.connection is not None:
-            if string_builder.connection==0:
-                response_headers += f"Connection: close\r\n"
-            if string_builder.connection==1:
-                response_headers += f"Connection: Keep-Alive\r\n"
-        else: response_headers += f"Connection: Keep-Alive\r\n"
+        if self.head_builder.connection is not None:
+            if self.head_builder.connection==0:
+                string_builder.append(f"Connection: close\r\n")
+            if self.head_builder.connection==1:
+                string_builder.append(f"Connection: Keep-Alive\r\n")
+        else: string_builder.append(f"Connection: Keep-Alive\r\n")
 
-        if string_builder.age is not None:
-            response_headers += f"Age: {string_builder.age}\r\n"
+        if self.head_builder.age is not None:
+            string_builder.append(f"Age: {self.head_builder.age}\r\n")
         else: pass # I don't know :(
         
-        if string_builder.date is not None:
-            response_headers += f"Date: {string_builder.date}\r\n"
+        if self.head_builder.date is not None:
+            string_builder.append(f"Date: {self.head_builder.date}\r\n")
         else: 
             current_datetime = datetime.datetime.now()
             http_date = current_datetime.strftime("%a, %d %b %Y %H:%M:%S GMT") # Not Sure!
-            response_headers += f"Date: {http_date}"
+            string_builder.append(f"Date: {http_date}\r\n")
 
 
-        if string_builder.x_cache_info is not None:
-            response_headers += f"X-Cache-Info: {string_builder.x_cache_info}\r\n"
+        if self.head_builder.x_cache_info is not None:
+            string_builder.append(f"X-Cache-Info: {self.head_builder.x_cache_info}\r\n")
         else: pass #response_headers += f"X-Cache-Info: caching\r\n" #not sure
 
-        if string_builder.content_length is not None:
-            response_headers += f"Content-Length: {string_builder.content_length}\r\n"
+        if self.head_builder.content_length is not None:
+            string_builder.append(f"Content-Length: {self.head_builder.content_length}\r\n")
         else: pass #response_headers += f"Content-Length: 0\r\n"
 
         self.initialize_headbuilder()# reset the headbuilder
-        return response_headers
+        return ''.join(string_builder)
 
