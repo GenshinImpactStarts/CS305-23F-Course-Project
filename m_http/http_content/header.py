88365476dfd9a1f9215e4c66d4f3060b7641aad2
Calmness <1628281262@qq.com>
2023-12-22 21:04:03 +0800

    feat(client.py): make a simple client.py

--- m_http/http_content/header.py
+++ m_http/http_content/header.py
@@ -10,38 +10,43 @@
 from ..status_code import StatusCode
 import datetime
 
-__all__ = 'Header',
+__all__ = ("Header",)
+
 
 # No need for you to call
 class HeadBuilder:
     def __init__(self):
         # the fields you must give
-        self.status_code=None
-        self.server_name=None
-        self.content_type=None
-        self.content_length=None
-        self.connection=None
-        self.x_cache_info=None
-        self.date=None
-        self.age=None
-        self.keep_alive=None
+        self.status_code = None
+        self.server_name = None
+        self.content_type = None
+        self.content_length = None
+        self.connection = None
+        self.x_cache_info = None
+        self.date = None
+        self.age = None
+        self.keep_alive = None
         # optional fields
+
 
 class Headers:
     class AcceptLanguage:
-        def __init__(self,header):
-            self.top_priority=None
-            self.possible_options=[] # [(lang, weight),(lang, weight),and so on] by weight DESC
-            languages = header.split(',')
-            if header==None:pass
+        def __init__(self, header):
+            self.top_priority = None
+            self.possible_options = (
+                []
+            )  # [(lang, weight),(lang, weight),and so on] by weight DESC
+            languages = header.split(",")
+            if header == None:
+                pass
             for language in languages:
-                parts = language.strip().split(';')
+                parts = language.strip().split(";")
                 lang = parts[0].strip()
                 if len(parts) > 1:
-                    weight = float(parts[1].split('=')[1])
+                    weight = float(parts[1].split("=")[1])
                 else:
                     weight = 1.0
-                    
+
                 if self.top_priority is None or weight > self.top_priority[1]:
                     self.top_priority = (lang, weight)
                 self.possible_options.append((lang, weight))
@@ -64,99 +69,112 @@ class Headers:
         self.referer = None
         self.user_agent = None
 
+
 def http_header_to_python(header):
     return header.lower().replace("-", "_")
 
 
 class Header:
-
-    server_name=None
+    server_name = None
 
     def initialize_headbuilder(self):
-        self.head_builder=HeadBuilder()
-        self.head_builder.server_name=self.server_name
+        self.head_builder = HeadBuilder()
+        self.head_builder.server_name = self.server_name
         pass
 
-    
     def __init__(self, server_name="GenShinImpactStarts"):
-        self.server_name=server_name
+        self.server_name = server_name
         self.initialize_headbuilder()
         pass
 
-
     def get_headbuilder(self):
         return self.head_builder
-    
+
     # only parse the Request-Line!
     def parse_request_line(request):
-        lines = request.split('\r\n')
+        lines = request.split("\r\n")
         request_line = lines[0]
-        method, path, version = [v.strip() for v in request_line.split(' ')]
+        method, path, version = [v.strip() for v in request_line.split(" ")]
         return method, path, version
-    
+
     # parse the others!
 
     def parse_request_headers(request):
-        if '\r\n\r\n' in request:
-            header_part, body_part = request.split('\r\n\r\n', 1)
+        if "\r\n\r\n" in request:
+            header_part, body_part = request.split("\r\n\r\n", 1)
         else:
             header_part = request
-            body_part = '' 
-        lines = header_part.split('\r\n')
+            body_part = ""
+        lines = header_part.split("\r\n")
         headers = Headers()
         for line in lines[1:]:
-            if line == '':
+            if line == "":
                 break
-            key, value = line.split(': ', 1)
-            if(http_header_to_python(key.strip())=='accept_charset'):
-                headers.accept_charset=[v.strip() for v in value.split(',')]
-            elif(http_header_to_python(key.strip())=='accept_language'):
-                headers.accept_language=Headers.AcceptLanguage(value.strip())
-            else:setattr(headers, http_header_to_python(key.strip()), value.strip())
+            key, value = line.split(": ", 1)
+            if http_header_to_python(key.strip()) == "accept_charset":
+                headers.accept_charset = [v.strip() for v in value.split(",")]
+            elif http_header_to_python(key.strip()) == "accept_language":
+                headers.accept_language = Headers.AcceptLanguage(value.strip())
+            else:
+                setattr(headers, http_header_to_python(key.strip()), value.strip())
 
-        return headers,body_part
-    
+        return headers, body_part
+
     # before you use this method, please edit the headbuilder correctly!
     def generate_response_headers(self):
-        string_builder=[]
-        string_builder.append(f"HTTP/1.1 {self.head_builder.status_code} {StatusCode.get_description(self.head_builder.status_code)}\r\n") 
+        string_builder = []
+        string_builder.append(
+            f"HTTP/1.1 {self.head_builder.status_code} {StatusCode.get_description(self.head_builder.status_code)}\r\n"
+        )
         string_builder.append(f"Server: {self.head_builder.server_name}\r\n")
 
         if self.head_builder.content_type is not None:
-            string_builder.append(f"Content-Type: {self.head_builder.content_type}\r\n") # maybe 'text/html; charset=utf-8'
-        else: string_builder.append(f"Content-Type: text/html; charset=utf-8\r\n")
+            string_builder.append(
+                f"Content-Type: {self.head_builder.content_type}\r\n"
+            )  # maybe 'text/html; charset=utf-8'
+        else:
+            string_builder.append(f"Content-Type: text/html; charset=utf-8\r\n")
 
         if self.head_builder.keep_alive is not None:
-            string_builder.append(f"Keep-Alive: timeout={self.head_builder.keep_alive[0]}, max={self.head_builder.keep_alive[1]}\r\n") # like 'timeout=5, max=1000'
-        else: pass # Who cares?
+            string_builder.append(
+                f"Keep-Alive: timeout={self.head_builder.keep_alive[0]}, max={self.head_builder.keep_alive[1]}\r\n"
+            )  # like 'timeout=5, max=1000'
+        else:
+            pass  # Who cares?
 
         if self.head_builder.connection is not None:
-            if self.head_builder.connection==0:
+            if self.head_builder.connection == 0:
                 string_builder.append(f"Connection: close\r\n")
-            if self.head_builder.connection==1:
+            if self.head_builder.connection == 1:
                 string_builder.append(f"Connection: Keep-Alive\r\n")
-        else: string_builder.append(f"Connection: Keep-Alive\r\n")
+        else:
+            string_builder.append(f"Connection: Keep-Alive\r\n")
 
         if self.head_builder.age is not None:
             string_builder.append(f"Age: {self.head_builder.age}\r\n")
-        else: pass # I don't know :(
-        
+        else:
+            pass  # I don't know :(
+
         if self.head_builder.date is not None:
             string_builder.append(f"Date: {self.head_builder.date}\r\n")
-        else: 
+        else:
             current_datetime = datetime.datetime.now()
-            http_date = current_datetime.strftime("%a, %d %b %Y %H:%M:%S GMT") # Not Sure!
+            http_date = current_datetime.strftime(
+                "%a, %d %b %Y %H:%M:%S GMT"
+            )  # Not Sure!
             string_builder.append(f"Date: {http_date}\r\n")
-
 
         if self.head_builder.x_cache_info is not None:
             string_builder.append(f"X-Cache-Info: {self.head_builder.x_cache_info}\r\n")
-        else: pass #response_headers += f"X-Cache-Info: caching\r\n" #not sure
+        else:
+            pass  # response_headers += f"X-Cache-Info: caching\r\n" #not sure
 
         if self.head_builder.content_length is not None:
-            string_builder.append(f"Content-Length: {self.head_builder.content_length}\r\n")
-        else: pass #response_headers += f"Content-Length: 0\r\n"
+            string_builder.append(
+                f"Content-Length: {self.head_builder.content_length}\r\n"
+            )
+        else:
+            pass  # response_headers += f"Content-Length: 0\r\n"
 
-        self.initialize_headbuilder()# reset the headbuilder
-        return ''.join(string_builder)
-
+        self.initialize_headbuilder()  # reset the headbuilder
+        return "".join(string_builder)
